2f0849c55f57cedc9f37347e2b3ab61e687e0192
yamagao <yamagao@126.com>
2015-10-13 14:59:44 -0400

    get content

--- template-mapsHover.php
+++ template-mapsHover.php
@@ -163,5 +163,15 @@ function isInfoWindowOpen(infoWindows){
 }
 google.maps.event.addDomListener(window, 'load', initialize);
 </script>
-
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	<article id="post-<?php the_ID(); ?>" <?php post_class( 'sd-full-width clearfix' ); ?>> 
+		<div class="entry-content">
+			<?php the_content(); ?>
+		</div>
+		<!-- entry-content --> 
+	</article>
+	<!-- sd-full-width -->
+<?php endwhile; else: ?>
+	<p><?php _e( 'Sorry, no posts matched your criteria', 'sd-framework' ) ?>.</p>
+<?php endif; ?>
 <?php get_footer(); ?>
